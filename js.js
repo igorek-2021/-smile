5fbf3a7508033c9c4f13fa6d665d8f6434dd4ed2
igorek-2021 <91989947+igorek-2021@users.noreply.github.com>
2021-10-05 23:56:26 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,9 +2,11 @@
 
 let button = document.querySelector('.button');
 let popup = document.querySelector('.modal');
+let popups = document.querySelector('.modal-1');
 
 button.addEventListener('click', function (evt) {
    evt.preventDefault();
    popup.classList.add("modal-show");
+   popups.classList.add("modal-shows");
 })
 
